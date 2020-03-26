cac5e76558366c118dd675a65a97bde7a12f0fea
derp-derp-derp <derpes@protonmail.com>
2020-03-26 13:45:20 -0400

    create addStudents method

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ class App extends React.Component {
     });
   }
 
-  addStudent(studentName) {
+  addStudents(studentName) {
     this.setState({
       students: this.state.students.concat(studentName)
     });
